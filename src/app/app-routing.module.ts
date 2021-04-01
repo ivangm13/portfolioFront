ffac32bce5180a671885751d3a84c8c2a202d78d
Iván García Muñoz <61294676+ivangm13@users.noreply.github.com>
2021-04-01 17:30:27 +0200

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from "./components/home/home.component";
+import { ViewProjectComponent } from './components/view-project/view-project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent,pathMatch:'full'},
+  {path:"contact",component:ContactComponent},
+  {path:"proyects/:id",component:ViewProjectComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/project.ts
@@ -0,0 +1,11 @@
+export class Project {
+    titulo: string;
+    descripcion: string;
+    imagenes: [];
+    url:string;
+    fecha: number;
+    cliente:string;
+    urlCliente:string;
+    categoría: string;
+    tecnologías: string;  
+}
